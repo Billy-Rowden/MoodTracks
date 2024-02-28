303f938da6ca56033d27a3a95410c6a0bd5787aa
eatontechnology <eaton.technology@gmail.com>
2024-02-28 19:58:51 +0000

    added general items for navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,24 @@
-// imports from bootstrap here
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Navbar() {
     return (
-        // Navbar content here
-    )
+        <Navbar expand="lg" className="customNav">
+            <Container>
+                <Navbar.Brand id="navTitle" href="/">
+                    {/* logo here */}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link className="navLink" href="/">login</Nav.Link>
+                        <Nav.Link className="navLink" href="/">contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Navbar
