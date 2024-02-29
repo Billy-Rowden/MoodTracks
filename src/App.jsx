32ef3abafc44a5eca1c7a75c919b61a16e65d2ec
eatontechnology <eaton.technology@gmail.com>
2024-02-29 15:23:28 +0000

    added general layout of page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import JournalNavbar from './components/Navbar'
 import JournalFooter from './components/Footer'
-import { Login, Contact } from './components/Pages'
+import { Login, Contact, Journal } from './components/Pages'
 import './App.css'
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <JournalNavbar />
       <Routes>
         <Route path='*' element={< Login />} />
+        <Route path='/journal' element={< Journal />} />
         <Route path='/contact' element={< Contact />} />
       </Routes>
       <JournalFooter />
@@ -20,3 +21,5 @@ function App() {
 }
 
 export default App
+
+// ToDo: Make Navbar and footer hidden on login page
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,2 +1,3 @@
 export { default as Login } from './Login'
 export { default as Contact } from './Contact'
+export { default as Journal } from './Journal'
