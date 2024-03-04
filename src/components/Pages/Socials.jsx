e23351a41daa625c18bdc76ded0c1398dbf9fdac
eatontechnology <eaton.technology@gmail.com>
2024-03-04 16:09:58 +0000

    formatted contact page

--- src/components/Pages/Socials.jsx
+++ src/components/Pages/Socials.jsx
@@ -3,39 +3,35 @@ import Container from 'react-bootstrap/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXTwitter, faInstagramSquare, faWhatsapp, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import MTLogoVar5 from '../../assets/images/MTLogoVar5.png'
 
 function Socials() {
     return (
         <>
+        <img src={MTLogoVar5} />
             <h1>Social Media</h1>
+            <Container class="socialMedia">
             <Card>
                 <a href="https://instagram.com" target="_blank">
                     <FontAwesomeIcon icon={faInstagramSquare} />
                 </a>
-            </Card>
-            <Card>
                 <a href="https://tiktok.com" target="_blank">
                     <FontAwesomeIcon icon={faTiktok} />
                 </a>
-            </Card>
-            <Card>
                 <a href="https://twitter.com" target="_blank">
                     <FontAwesomeIcon icon={faXTwitter} />
                 </a>
             </Card>
-            <Container>
+            </Container>
+            <Container className="contacts">
                 Got Feedback? Let us know!
                 <Card>
                     <a href="https://wa.me/whatsappphonenumber" target="_blank">
                         <FontAwesomeIcon icon={faWhatsapp} />
                     </a>
-                </Card>
-                <Card>
                     <a href="tel:+1234567890">
                         <FontAwesomeIcon icon={faPhone} />
                     </a>
-                </Card>
-                <Card>
                     <a href="mailto:contact@moodtracks.com">
                         <FontAwesomeIcon icon={faEnvelope} />
                     </a>
