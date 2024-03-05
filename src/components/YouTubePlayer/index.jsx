c3f03b4739bfd06f565b5138e31e0cc36504865f
eatontechnology <eaton.technology@gmail.com>
2024-03-05 15:16:18 +0000

    added functionality for youtube api instead

---
+++ src/components/YouTubePlayer/index.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const apiKey = "AIzaSyDCQiak0679rpJunPy7J-WSy8OcmPJkXnM";
+
+const YoutubePlayer = ({ emotion }) => {
+    const [playlist, setPlaylist] = useState(null);
+
+    useEffect(() => {
+        const fetchVideoUrl = async () => {
+            try {
+                let query = ''; // Initialize the query string
+
+                // Set the query based on the emotion
+                switch (emotion) {
+                    case 'good':
+                        query = 'chill instrumentals uplifting';
+                        break;
+                    case 'neutral':
+                        query = 'neutral instrumentals';
+                        break;
+                    case 'low':
+                        query = 'sad lofi instrumental';
+                        break;
+                    case 'frustrated':
+                        query = 'meditation instrumental';
+                        break;
+                    default:
+                        console.error('Invalid emotion:', emotion);
+                        return;
+                }
+
+                const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        part: 'snippet',
+                        q: query,
+                        type: 'playlist',
+                        key: apiKey
+                    }
+                });
+
+                if (response.data.items.length > 0) {
+                    const playlistId = response.data.items[0].id.playlistId;
+                    setPlaylist(`https://www.youtube.com/embed/videoseries?list=${playlistId}&autoplay=1`);
+                } else {
+                    console.error('No playlist found for the emotion:', emotion);
+                }
+            } catch (error) {
+                console.error('Error fetching video URL:', error);
+            }
+        };
+
+        fetchVideoUrl();
+    }, [emotion]);
+
+    return (
+        <div style={{ paddingTop: '20px' }}>
+            {playlist && (
+                <iframe 
+                    width="560"
+                    height="315"
+                    src={playlist}
+                    frameBorder="0"
+                    title="Playlist Player"
+                ></iframe>
+            )}
+        </div>
+    );
+};
+
+export default YoutubePlayer;
