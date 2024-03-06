520613d3fed615eecdcb85b8b87c0ba324962c19
Mo Rahman <mohammed.rahman676@gmail.com>
2024-03-06 14:51:08 +0000

    assigned spotifyplaylist variables for authentication

--- src/components/Spotify/spotifyPlayer.jsx
+++ src/components/Spotify/spotifyPlayer.jsx
@@ -3,3 +3,8 @@ import axios from 'axios';
 
 const apiKey = '8e3c5e197d0848edb1aa1d706f26fd97';
 
+const SpotifyPlaylists = ({ emotion }) => {
+    const [accessToken, setAccessToken] = useState('');
+    const [playlist, setPlaylist] = useState(null);
+    const [error, setError] = useState('');
+};
