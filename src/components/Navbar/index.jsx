ebbe13a522566223f2116cc9cda0441788f2bf11
eatontechnology <eaton.technology@gmail.com>
2024-03-06 11:37:52 +0000

    pressing logout on navbar clears local storage

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import mainLogo from '../../assets/images/mainLogo.png'; // Import the image file
+import mainLogo from '../../assets/images/mainLogo.png';
 import './index.css';
 
 function JournalNavbar() {
+    const handleLogout = () => {
+        localStorage.clear();
+    };
+
     return (
         <Navbar expand="lg" className="customNav">
             <Navbar.Brand id="navTitle">
@@ -13,7 +17,7 @@ function JournalNavbar() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link className="navLink" href="/">Logout</Nav.Link>
+                    <Nav.Link className="navLink" href="/" onClick={handleLogout}>Logout</Nav.Link>
                     <Nav.Link className="navLink" href="/journal">Journal</Nav.Link>
                     <Nav.Link className="navLink" href="/contact">Contact</Nav.Link>
                     <Nav.Link className="navLink" href="/socials">Socials</Nav.Link>
